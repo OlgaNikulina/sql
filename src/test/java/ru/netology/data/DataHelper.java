10bcc297c2c43182c0943bdf484966c6cf94a063
OlgaNikulina <nikulinaov@inbox.ru>
2020-06-04 00:53:46 +0500

    DataHelper, LoginPage, VerificationPage, LoginTest added

---
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -0,0 +1,46 @@
+package ru.netology.data;
+
+import lombok.Value;
+import lombok.val;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DataHelper {
+    private DataHelper() {
+    }
+
+    @Value
+    public static class AuthInfo {
+        private String login;
+        private String password;
+    }
+
+    public static AuthInfo getAuthInfo() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+    @Value
+    public static class VerificationCode {
+        private String code;
+    }
+
+    public static void getVerificationCode() throws SQLException {
+        val authCodeSQL = "SELECT  code FROM auth_codes";
+
+        try (
+                val conn = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/app", "app", "pass"
+                );
+                val authCodeStmt = conn.prepareStatement(authCodeSQL);
+        ) {
+            try (val rs = authCodeStmt.executeQuery(authCodeSQL)) {
+                while (rs.next()) {
+                    val code = rs.getString("code");
+                    System.out.println(code);
+                }
+            }
+        }
+    }
+}
+
---
+++ src/test/java/ru/netology/test/LoginTest.java
@@ -0,0 +1,22 @@
+package ru.netology.test;
+
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.page.LoginPage;
+import ru.netology.page.VerificationPage;
+
+import java.sql.SQLException;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class LoginTest {
+
+    @Test
+    void shouldLogin() throws SQLException {
+        val loginPage = open("http://localhost:9999)", LoginPage.class);
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationCode = DataHelper.getVerificationCode();
+        val verificationPage = VerificationPage.validVerify;
+    }
+}
